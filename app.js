dbc002ba26ab2ee8827d42c376d175a489a7aa68
Lance Boer <lance@Lances-MacBook-Pro-2.local>
2021-07-27 14:11:53 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//Below, if the URL already has a #, run the getProject function
+if(location.hash) {
+	var hash = location.hash.substr(1); //Get the URL after the # sign
+	getProject();
+}
+
+//Below, when the # in the URL changes, call the getProject function
+window.onhashchange = function() {
+	getProject();
+}
+
+function getProject() {
+	var hash = location.hash.substr(1); //Update the variable
+	// Below, get the HTML file with a filename that matches the hash variable
+	var xhr= new XMLHttpRequest();
+	xhr.open('GET', `${hash}.html`, true);
+	xhr.onreadystatechange= function() {
+		if (this.readyState!==4) return;
+		if (this.status!==200) return; // or whatever error handling you want
+		document.getElementById('replaceContent').innerHTML= this.responseText;
+	};
+	xhr.send();
+};
